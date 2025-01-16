a3f5fb555dad49b1b1bdaa03e14b3736fb0c19d9
KH-taehyuk <warsfac@gmail.com>
2025-01-16 17:48:42 +0900

    250116 집에서 ajax 더 해야됨

--- Sinuedu/src/main/java/com/sinuedu/board/lecture/controller/LectureController.java
+++ Sinuedu/src/main/java/com/sinuedu/board/lecture/controller/LectureController.java
@@ -1,11 +1,13 @@
 package com.sinuedu.board.lecture.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.sinuedu.board.lecture.model.service.LectureService;
 import com.sinuedu.board.lecture.model.vo.Lecture;
@@ -21,14 +23,23 @@ public class LectureController {
 	private final LectureService cService;
 	
 	@GetMapping("list")
-	public String selectLectureList() {
+	public ModelAndView selectLectureList(ModelAndView mv) {
 		
 		ArrayList<Lecture> list = cService.selectLectureList();
 		
 		for(Lecture lec : list) {
-			System.out.println(lec);
+//			System.out.println(lec);
+			int lecNo = lec.getLecNo();
+			int capCount = cService.captherCount(lecNo);
+			lec.setTotalChap(capCount);
 		}
 		
-		return "views/classes/category";
+		
+		
+		
+		
+		mv.addObject("list", list).setViewName("views/classes/category");
+		
+		return mv;
 	}
 }
